417fe6c3ec77074223ccd21bb5337c651120c10e
pandaman64 <kointosudesuyo@infoseek.jp>
2022-09-18 13:12:53 +0900

    Emit cache hit

--- benchmarks/direct.js
+++ benchmarks/direct.js
@@ -1,5 +1,8 @@
 import http from "k6/http";
 import { randomSeed } from "k6";
+import { Counter } from "k6/metrics";
+
+const cache_hit_metrics = new Counter("cache_hit");
 
 export let options = {
     vus: "30",
@@ -10,4 +13,8 @@ export default function() {
     randomSeed(__VU * 31 + __ITER);
     let number = Math.floor(Math.random() * 10000);
     let response = http.get(`http://localhost:8000/steps/${number}`);
+    let cache_hit = response.json()["cache_hit"];
+    if (cache_hit) {
+        cache_hit_metrics.add(1)
+    }
 };
